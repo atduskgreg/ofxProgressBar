75b5ba277854c69b3d93794e7866d3be768408f8
Greg Borenstein <greg@mfdz.com>
2012-08-13 10:02:21 -0400

    methods for setting label, value, and max directly

--- src/ofxProgressBar.cpp
+++ src/ofxProgressBar.cpp
@@ -29,8 +29,14 @@ ofxProgressBar::ofxProgressBar(int _x, int _y, int _w, int _h, int * _value, int
 void ofxProgressBar::init(){
     backgroundColor = ofColor(125);
     barColor = ofColor(90);
+    fontColor = ofColor(90);
+    
+    labelPosition.x = x+3;
+    labelPosition.y = y-10;
+    
     borderSize = 1;
     hidden = false;
+    label = "";
 }
 
 float ofxProgressBar::progress(){
@@ -61,20 +67,50 @@ void ofxProgressBar::setBorderSize(int b){
     borderSize = b;
 }
 
+void ofxProgressBar::setLabel(string labelText){
+    cout << labelText << endl;
+    label = labelText;
+}
+
+void ofxProgressBar::setLabelPosition(ofVec2f p){
+    labelPosition = p;
+}
+
+void ofxProgressBar::setFont(ofTrueTypeFont f){
+    font = f;
+}
+
+void ofxProgressBar::setFontColor(ofColor c){
+    fontColor = c;
+}
+
+void ofxProgressBar::setProgress(int p){
+    *value = p;
+}
+
+void ofxProgressBar::setMax(int m){
+    if(useSavedMax){
+        savedMax = m;
+    } else {
+        *max = m;    
+    }
+}
+
 void ofxProgressBar::draw(){
     if(!hidden){
         ofPushStyle();
+        ofSetColor(fontColor);
+        font.drawString(label, labelPosition.x, labelPosition.y);
+        
         ofSetColor(backgroundColor);
         ofFill();
         ofRect(x, y, w, h);
+        
         ofSetColor(barColor);
-        
         float width = ofClamp(progress() * (w-(2*borderSize)), 0, w-(2*borderSize));
-        
         ofRect(x+borderSize, y+borderSize, width, h-(2*borderSize));
         ofPopStyle();
     }
-    
 }
 
 
--- src/ofxProgressBar.h
+++ src/ofxProgressBar.h
@@ -2,6 +2,9 @@
 
 #include "ofMain.h"
 
+// TODO:
+// - time-to-complettion estimate
+
 class ofxProgressBar {
 
 public:
@@ -9,6 +12,8 @@ public:
     ofxProgressBar(int x, int y, int w, int h, int * value, int max);
     ofxProgressBar(int x, int y, int w, int h, int * value, int * max);
 
+    //ofxProgressBar( ofxProgressBar& other );
+    
     float progress(); //0.0-1.0
     void draw();
     void hide();
@@ -17,6 +22,23 @@ public:
     void setBackgroundColor(ofColor c);
     void setBarColor(ofColor c);
     void setBorderSize(int b);
+    void setLabel(string labelText);
+    void setFont(ofTrueTypeFont f);
+    void setFontColor(ofColor c);
+    void setLabelPosition(ofVec2f p);
+    
+    // Use these to set the progress and max of the bar
+    // directly
+    void setProgress(int p);
+    void setMax(int m);
+    
+    int getMax(){ 
+        if(useSavedMax){
+            return savedMax;
+        } else {
+            return *max; 
+        }
+    }
     
 private:
     int x,y,w,h;
@@ -27,8 +49,14 @@ private:
     ofColor barColor;
     bool hidden;
     
+    string label;
+    
     int savedMax;
     bool useSavedMax;
+    
+    ofTrueTypeFont font;
+    ofColor fontColor;
+    ofVec2f labelPosition;
     
     void init();
 };
